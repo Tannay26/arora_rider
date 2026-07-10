6dd2560fab6d1bbffa3e46ea2c0f88428d065898
Tannay26 <tannay26arora@gmail.com>
2026-07-10 17:59:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 (() => {
   "use strict";
 
-  window.addEventListener("load", () => {
+  window.addEventListener("load", async () => {
+    try {
+      await AR.loadAssets();
+    } catch (error) {
+      console.warn("Some visual assets could not be loaded.", error);
+    }
+
     const game = new AR.Game();
     window.AR_GAME = game;
   });
